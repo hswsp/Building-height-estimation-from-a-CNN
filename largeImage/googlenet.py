b140a6af7ae525ab2bd404cf398ff8944afcda3f
hswsp <hswsp@mail.ustc.edu.cn>
2018-05-18 19:38:48 +0800

    661

--- largeImage/googlenet.py
+++ largeImage/googlenet.py
@@ -75,28 +75,29 @@ def load_largeData(X_depths,img_type):
     print dsm_num
     if img_type == 'depths':
         # not norm
-        X_depths1 = np.array(X_depths[:dsm_num/4]).astype(np.float32)
-        X_depths2 = np.array(X_depths[dsm_num / 4:dsm_num / 2]).astype(np.float32)
+        X_depths1 = np.array(X_depths[:dsm_num/4])
+        X_depths2 = np.array(X_depths[dsm_num / 4:dsm_num / 2])
         X1 = np.concatenate((X_depths1,X_depths2), axis = 0)
         del X_depths1,X_depths2
-        X_depths3 = np.array(X_depths[dsm_num / 2:3 * dsm_num / 4]).astype(np.float32)
-        X_depths4 = np.array(X_depths[3 * dsm_num / 4:]).astype(np.float32)
+        X_depths3 = np.array(X_depths[dsm_num / 2:3 * dsm_num / 4])
+        X_depths4 = np.array(X_depths[3 * dsm_num / 4:])
         X2 = np.concatenate((X_depths3, X_depths4), axis=0)
         del X_depths3, X_depths4
         X_depths_train = np.concatenate((X1, X2), axis=0)
         X_depths_train = np.array(X_depths_train)
     elif img_type == 'images':
         # have been norm
-        X_depths1 = np.array(X_depths[:dsm_num / 4]).astype(np.float32)
-        X_depths2 = np.array(X_depths[dsm_num / 4:dsm_num / 2]).astype(np.float32) # must be float32 in order to divide
-        X1 = normalization(np.concatenate((X_depths1, X_depths2), axis=0))
+        X_depths1 = np.array(X_depths[:dsm_num / 4])                                #.astype(np.float32)
+        X_depths2 = np.array(X_depths[dsm_num / 4:dsm_num / 2]) # must be float32 in order to divide
+        # X1 = normalization(np.concatenate((X_depths1, X_depths2), axis=0))
         del X_depths1, X_depths2
         X_depths3 = np.array(X_depths[dsm_num / 2:3 * dsm_num / 4]).astype(np.float32)
         X_depths4 = np.array(X_depths[3 * dsm_num / 4:]).astype(np.float32)
-        X2 = normalization(np.concatenate((X_depths3, X_depths4), axis=0))
+        # X2 = normalization(np.concatenate((X_depths3, X_depths4), axis=0))
         del X_depths3, X_depths4
         X_depths_train = np.concatenate((X1, X2), axis=0)
         X_depths_train = np.array(X_depths_train)
+        X_depths_train = normalization(X_depths_train)
 
     return X_depths_train
 
