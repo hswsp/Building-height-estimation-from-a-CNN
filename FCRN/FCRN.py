bf602b3c89413015f0b301a392c78a42dcd18595
hswsp <hswsp@mail.ustc.edu.cn>
2018-05-23 20:03:31 +0800

    66

--- FCRN/FCRN.py
+++ FCRN/FCRN.py
@@ -93,11 +93,12 @@ def generate_arrays_from_file(input_paths,batch_size):
                 # X = misc.imresize(X,0.5)# input is 512
                 Y = np.array(Y) 
                 Y = Y[:,:,:,0] # only take one channel!
-                Y = np.expand_dims(Y,axis=4) # output is (?,?,?,?)
+                
                 print Y.shape
                 for i in range(2):
                     # Y = misc.imresize(Y,0.5)#output is 256
                     Y = np.array([cv2.pyrDown(Y[i]) for i in range(len(Y))])
+                Y = np.expand_dims(Y,axis=-1) # output is (?,?,?,?)
                 print Y.shape
                 X = rescale(X)
                 Y = rescale(Y) 
