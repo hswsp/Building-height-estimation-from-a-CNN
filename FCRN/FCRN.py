a1ad982f04f520b4e4ada49f24098d0347e47b5e
hswsp <hswsp@mail.ustc.edu.cn>
2018-05-24 22:41:55 +0800

    66

--- FCRN/FCRN.py
+++ FCRN/FCRN.py
@@ -95,7 +95,7 @@ def generate_arrays_from_file(input_paths,batch_size):
                 Y = np.array(Y) 
                 Y = Y[:,:,:,0] # only take one channel!
                 # print Y.shape
-                for i in range(2):
+                for i in range(1):
                     # Y = misc.imresize(Y,0.5)#output is 256
                     Y = np.array([cv2.pyrDown(Y[i]) for i in range(len(Y))])
                 Y = np.expand_dims(Y,axis=-1) # output is (?,?,?,?)
@@ -123,6 +123,86 @@ def load_data(input_dir):
     # DSM_paths = sorted(DSM_paths)
     return input_paths,len(input_paths)
 
+def identity_block_last(input_tensor, kernel_size, filters, stage, block):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1),dilation_rate=2, name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size,dilation_rate=2,
+               padding='same', name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), dilation_rate=2,name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    x = layers.add([x, input_tensor])
+    x = Activation('relu')(x)
+    return x
+
+
+def conv_block_last(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+    """A block that has a conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+        strides: Strides for the first conv layer in the block.
+    # Returns
+        Output tensor for the block.
+    Note that from stage 3,
+    the first conv layer at main path is with strides=(2, 2)
+    And the shortcut should have strides=(2, 2) as well
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), dilation_rate=2,strides=strides,
+               name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size, dilation_rate=2,padding='same',
+               name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), dilation_rate=2,name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = Conv2D(filters3, (1, 1), strides=strides,
+                      name=conv_name_base + '1')(input_tensor)
+    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = layers.add([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
 # net definition
 def Up_Projection(x,f,num):
 
@@ -146,8 +226,14 @@ def FCRN(model_name):
                                                       weights=None,pooling=None)
     #pop the last avepooling                                                  
     base_model.layers.pop()
-    last = base_model.layers[-1].output
-    x = Conv2D(1024, (1, 1), name='con2D_1', padding="same")(last)
+    x = base_model.layers['activation_40'].output
+    # 最后只缩小16倍！=32
+    x = conv_block_last(x, 3, [512, 512, 2048], stage=5, block='a',strides=(1,1))
+    x = identity_block_last(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block_last(x, 3, [512, 512, 2048], stage=5, block='c')
+
+    
+    x = Conv2D(1024, (1, 1), name='con2D_1', padding="same")(x)
     x = BatchNormalization(axis=-1)(x)
     #1024->64
     nb_conv = [9,8,7,6]
